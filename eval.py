fe74cd0280b3ee0eed94f95abededbe75440d455
takeshi8989 <htakeshi0614@gmail.com>
2025-01-13 18:55:15 -0800

    fix env name

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import pickle
 
-from env import Go2Env
+from env import HumanoidWalkEnv
 from rsl_rl.runners import OnPolicyRunner
 
 import genesis as gs
@@ -23,7 +23,7 @@ def main():
     env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"{log_dir}/cfgs.pkl", "rb"))
     reward_cfg["reward_scales"] = {}
 
-    env = Go2Env(
+    env = HumanoidWalkEnv(
         num_envs=1,
         env_cfg=env_cfg,
         obs_cfg=obs_cfg,
